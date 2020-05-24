5bb6fce2d44985533f6f7084f254937279018b65
pranavv481 <pranavv481@gmail.com>
2020-05-24 10:46:58 +0530

    ref child class

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import FocusInput from './components/FocusInput';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <FocusInput />
+      </div>
+    );
+  }
+}
+
+export default App;
